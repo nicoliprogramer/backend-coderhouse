1fc69e60935175590dfa4b9256bfb70fdad6ffc9
nicoliprogramer <imotivacionesf@gmail.com>
2022-12-07 22:16:42 -0300

    pf

--- clase12/desafio/public/index.js
+++ clase12/desafio/public/index.js
@@ -4,28 +4,29 @@ const socketClient = io();
 
 const productForm = document.getElementById("productForm");
 
-productForm.addEventListener("submit", (event)=>{
+productForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const product = {
         title: document.getElementById("title").value,
         price: document.getElementById("price").value,
         thumbnail: document.getElementById("thumbnail").value
     }
-    //Enviar producto por socket
+
     socketClient.emit("newProduct", product)
 })
 
 const productsContainer = document.getElementById("productsContainer");
 
-socketClient.on("products", async(data)=>{
+socketClient.on("products", async (data) => {
     console.log(data)
     const templateTable = await fetch("./templates/table.handlebars");
-    //Se convierte a formato del template
+
+    // se convierte a formato del template
     const templateFormat = await templateTable.text();
     const template = Handlebars.compile(templateFormat);
 
-    //  se genera el HTML con  el template y datos
-    const html = template({products: data})
+    //  se genera el HTML con  el template y datos  
+    const html = template({ products: data })
     productsContainer.innerHTML = html;
     console.log(html)
 })
@@ -46,14 +47,15 @@ socketClient.on("messagesChat", (data) => {
     chatContainer.innerHTML = messages
 })
 
+
 // user
 let user = "";
 Swal.fire({
-    title:"Bienvenido/a",
+    title: "Bienvenido/a",
     text: "Ingrese su email",
     input: "email",
     allowOutsideClick: false
-}).then(response =>{
+}).then(response => {
     console.log(response)
     user = response.value;
     document.getElementById("username").innerHTML = `<span class="author">Bienvenido ${user}</span>`;
@@ -65,7 +67,7 @@ const chatForm = document.getElementById("chatForm");
 
 const messageChat = document.getElementById("messageChat");
 
-chatForm.addEventListener("submit", (event)=>{
+chatForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const message = {
         date: new Date(Date.now()).toLocaleString(),
@@ -73,5 +75,5 @@ chatForm.addEventListener("submit", (event)=>{
         text: messageChat.value
     }
     socketClient.emit("newMsg", message)
-    messageChat.value="";
+    messageChat.value = "";
 })
